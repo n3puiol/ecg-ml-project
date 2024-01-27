dbcedbe5aba6b35be054ebff37e584e8eb30a54f
louis <louis.vanstappen@gmail.com>
2024-01-27 16:49:21 +0100

    correct assessment plot

--- load.py
+++ load.py
@@ -1,20 +1,28 @@
 import numpy as np
-from matplotlib import pyplot as plt
+
+from config import Config
 from create_dataset import MitDBDataset
 import tensorflow as tf
+
+from plot_data import plot_ecg
 
 tf.config.set_visible_devices([], 'GPU')
 
 
 def load():
     mit_dataset = MitDBDataset('./data/mitdb/1.0.0/', load=True)
+    config = Config()
     X, y = mit_dataset.load_dataset(feature='MLII')
 
     model = tf.keras.models.load_model('models/MLII-latest.hdf5')
     y_pred = model.predict(X[1:2])
     max_idx = np.argmax(y_pred[0])
+
     if np.isclose(y[1:2][0][max_idx], 1.0):
         print("Correct")
+        plot_ecg(X[1], y[1], config.classes)
+    else:
+        print("Incorrect")
 
 
 if __name__ == '__main__':
--- plot_data.py
+++ plot_data.py
@@ -1,14 +1,18 @@
+import numpy as np
 from matplotlib import pyplot as plt
 
+from config import Config
 from create_dataset import MitDBDataset
 
 
-def plot():
-    mit_dataset = MitDBDataset('./data/mitdb/1.0.0/', load=True)
-    X, y = mit_dataset.load_dataset(feature='MLII')
-    plt.plot(X[1])
+def plot_ecg(x, y, classes):
+    plt.plot(x)
+    plt.title("Class: " + classes[np.argmax(y)])
     plt.show()
 
 
 if __name__ == '__main__':
-    plot()
+    mit_dataset = MitDBDataset('./data/mitdb/1.0.0/', load=True)
+    X, y = mit_dataset.load_dataset(feature='MLII')
+    config = Config()
+    plot_ecg(X[0], y[0], config.classes)
