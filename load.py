11c0a4c65f5c0dd314dfabb72e864c0baed4b218
louis <louis.vanstappen@gmail.com>
2024-01-28 12:14:50 +0100

    testing all the data with model

--- load.py
+++ load.py
@@ -15,14 +15,32 @@ def load():
     X, y = mit_dataset.load_dataset(feature='MLII')
 
     model = tf.keras.models.load_model('models/MLII-latest.hdf5')
-    y_pred = model.predict(X[1:2])
-    max_idx = np.argmax(y_pred[0])
+    y_pred = model.predict(X)
+    num_correct = 0
+    num_incorrect = 0
+    for i in range(len(y_pred)):
+        max_idx = np.argmax(y_pred[i])
+        if np.isclose(y[i][max_idx], 1.0):
+            num_correct += 1
+        else:
+            num_incorrect += 1
 
-    if np.isclose(y[1:2][0][max_idx], 1.0):
-        print("Correct")
-        plot_ecg(X[1], y[1], config.classes)
-    else:
-        print("Incorrect")
+    print("Correct: ", num_correct)
+    print("Incorrect: ", num_incorrect)
+    print("Accuracy: ", num_correct / (num_correct + num_incorrect))
+
+
+
+    # max_idx = np.argmax(y_pred[0])
+
+
+
+    # if np.isclose(y[0][max_idx], 1.0):
+    #     print("Correct")
+    #     # plot_ecg(X[1], y[1], config.classes)
+    # else:
+    #     print("Incorrect")
+
 
 
 if __name__ == '__main__':
